83b8319f62d504bf449c2835033ab26df3d28613
djulie fontenele <agathaf691@gmail.com>
2026-07-16 17:05:13 -0300

    feat(produtos): adicionar funções set e computed para gerenciamento de estado

--- src/app/features/produtos/lista-produtos/lista-produtos.ts
+++ src/app/features/produtos/lista-produtos/lista-produtos.ts
@@ -1,9 +1,10 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, computed } from '@angular/core';
 import { Produto } from '../produto/produto';
+import { PrecoFormatadoPipe } from '../../../shared/pipes/preco-formatado-pipe';
 
 @Component({
   selector: 'app-lista-produtos',
-  imports: [Produto],
+  imports: [Produto, PrecoFormatadoPipe],
   templateUrl: './lista-produtos.html',
   styleUrl: './lista-produtos.css',
 })
@@ -39,6 +40,15 @@ adicionarProduto(){
     ...listaAtual, {nome: 'Sony PlayStation 5', preco:10000}
   ]);
 }
+totalProdutos = computed (() => this.produtos().length);
+valorTotal= computed (() => { return this.produtos().reduce
+  ((total, item) => total + item.preco,0)});
+
+  substituirProdutos (){
+    this.produtos.set([
+      {nome: 'Arroz Tio João', preco: 400},
+    ]);
+  }
 }
 
 
