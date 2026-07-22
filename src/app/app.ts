080f96a044fb21a4952ca5048c834a626cbf799c
djulie fontenele <manu.gibson14@gmail.com>
2026-07-22 16:15:30 -0300

    feat(app): integra o estado de autenticação ao componente principal e adiciona dados da loja

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,19 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet, RouterLink} from '@angular/router';
+import { UpperCasePipe } from '@angular/common';
+import { usuarioLogado, login, logout } from './core/auth';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, UpperCasePipe],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('e-commerce');
   nomeLoja = 'TechPrime'
+  usuarioLogado = usuarioLogado;
+  login = login;
+  logout = logout;
+
 }
